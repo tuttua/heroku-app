d575d13a7b654c0b7b51ff7c444145ae972ab2a8
tuttua <109905885+tuttua@users.noreply.github.com>
2022-07-24 22:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("Subtraction")
-minuend=st.number_input('Enter the minuend')
-subtrahend=st.number_input('Enter the subtrahend')
-difference=minuend-subtrahend
-st.write(minuend, ' - ',subtrahend, ' = ',difference)
+
+def user_input_features():
+    n1=st.number_input("Enter the minuend:")
+    n2=st.number_input("Enter the subtrahend:")
+    return n1,n2
+n1,n2=user_input_features()
+if st.button('Calculate'):
+    st.write(n1-n2)
